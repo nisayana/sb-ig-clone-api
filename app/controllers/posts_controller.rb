0783e7f8f18154f3b0085167ec2162a610ccc02e
nisayana <nimaevasb@gmail.com>
2021-11-01 17:43:38 -0400

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
 
+    def index
+        render json: Post.all
+    end
+
     def create
         byebug
+        post  = Post.new(caption: post_params["caption"])
+        post.avatar.attach(post_params["file"])
+        post.photo_url = url_for(post.avatar)
+
+        if post.save
+            render json: post
+        else
+            render json: post.errors
+        end
     end
+
+    private
+    def post_params
+        params.permit(:file, :caption) 
+    end 
 end
